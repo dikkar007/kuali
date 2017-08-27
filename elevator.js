3ce2d5db08ed66d10b1e0ce693dbf89558250bcd
sd <shirish007@gmail.com>
2017-08-27 19:18:07 -0400

    move function Req 2 Each elevator will report as is moves from floor to floor. Req 3 Each elevator will report when it opens or closes its doors.

--- elevator.js
+++ elevator.js
@@ -1,9 +1,44 @@
+
 module.exports = class Elevator {
 
   constructor (parent, index, startFloor) {
     this.index = index
     this.onFloor = startFloor
     this.isMoving = false
+
   }
 
+  move (from, to) {
+    this.isMoving = true
+
+    console.log('Req #2 On floor ' + this.onFloor)
+
+    if (this.onFloor == from) {
+      console.log('Req #3 Door open / closes on floor ' + this.onFloor)
+      if (to - from > 0) {
+        this.moveToDestUp(to)
+      } else {
+        this.moveToDestDown(to)
+      }
+      return
+    } else {
+      if (this.onFloor < from) {
+        ++this.onFloor
+      } else {
+        --this.onFloor
+      }
+      this.move(from, to)
+    }
+  }
+
+  moveToDestUp (to) {
+
+  }
+
+  moveToDestDown (to) {
+
+  }
+
+  onDestFloor () {
+  }
 }
