37d044e6c920161f8d42de48609bb32d708ed749
sd <shirish007@gmail.com>
2017-08-27 18:56:34 -0400

    initialize  elevators

--- elevator.js
+++ elevator.js
@@ -1,3 +1,10 @@
 module.exports = class Elevator {
 
+  constructor (parent, index, startFloor) {
+    this.index = index
+    this.onFloor = startFloor
+    this.isMoving = false
+
+  }
+
 }
--- elevatorController.js
+++ elevatorController.js
@@ -9,5 +9,11 @@ module.exports = class ElevatorController {
     this.minFloor = 1
 
     console.log('#1 Req > Elevators : ' + this.elevators + ' Total Floors : ' + this.floorsCount + ' Min Floor :' + this.minFloor)
+
+    // initialize  elevators:
+    this.elevators = Array.apply(null, Array(elevators))
+                          .map(function (value, index) {
+                            return new Elevator(self, index, 1)
+                          })
   }
 }
