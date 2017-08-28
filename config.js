a2f7f4eb643ef4c742839f0e314e7848150a847a
sd <shirish007@gmail.com>
2017-08-27 20:37:18 -0400

    console.log & testing

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 const ElevatorController = require('./elevatorController')
-
 class Config {
+
   constructor () {
+
+    console.log('----- Start : Kuali Elevator -----')
+
     var ec = new ElevatorController(4, 9) // number of elevators, number of floors
     ec.elevatorRequest(3, 8) // request from to
     ec.elevatorRequest(2, 7)
@@ -9,11 +12,19 @@ class Config {
     ec.elevatorRequest(1, 7)
     ec.elevatorRequest(2, 6)
 
+    ec.elevatorRequest(3, 8) // request from to
+    ec.elevatorRequest(2, 7)
+    ec.elevatorRequest(7, 2)
+    ec.elevatorRequest(1, 7)
+    ec.elevatorRequest(2, 6)
 
+
+  //  ec.elevatorRequest(8, 3) // request from to
     ec.elevatorRequest(8, 11) // request from to
     ec.elevatorRequest(8, -1) // request from to
     ec.elevatorRequest(4, 4) // request from to
-  }
-}
 
+    console.log('----- End : Kuali Elevator -----')
+  }
+ }
 new Config()
